b87883aa01cc5177fb29ef00a9156105a6a1627a
Ghassen Dhaoui <GhassenDhaoui@outlook.com>
2025-03-23 22:37:38 +0100

    change

--- HttpServer/Program.cs
+++ HttpServer/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HttpServer;
+
+public static class MainClass
+{
+    public static void Main(string[] args)
+    {
+        HttpServer server = new HttpServer();
+    }
+}
